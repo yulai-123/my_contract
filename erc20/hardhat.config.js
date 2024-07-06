8f8f346a0190e88ad25f96f9d72e2951137c80d2
thesky <thesky341@foxmail.com>
2024-07-06 12:21:28 +0800

    添加 网络配置

--- erc20/hardhat.config.js
+++ erc20/hardhat.config.js
@@ -1,8 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("../my_config.js");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   defaultNetwork: "base",
-
+  networks: {
+    base: {
+      url: myConfig.url,
+      chainId: myConfig.chainId,
+      accounts: myConfig.accounts
+    }
+  }
 };
